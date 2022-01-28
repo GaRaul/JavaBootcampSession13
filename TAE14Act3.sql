181a95b3b891f8bab017f79fcc099d23351852bc
Raúl <76182237+GaRaul@users.noreply.github.com>
2022-01-28 12:34:47 +0100

    Add files via upload

---
+++ TAE14Act3.sql
@@ -0,0 +1,87 @@
+CREATE DATABASE  IF NOT EXISTS `Tienda_de_Informatica` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `Tienda_de_Informatica`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 192.168.1.154    Database: Tienda_de_Informatica
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Articulos`
+--
+
+DROP TABLE IF EXISTS `Articulos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Articulos` (
+  `Codigo` int unsigned NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `Precio` int DEFAULT NULL,
+  `Fabricante` int unsigned NOT NULL,
+  PRIMARY KEY (`Codigo`),
+  KEY `Fabricante` (`Fabricante`),
+  CONSTRAINT `Articulos_ibfk_1` FOREIGN KEY (`Fabricante`) REFERENCES `Fabricantes` (`Codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Articulos`
+--
+
+LOCK TABLES `Articulos` WRITE;
+/*!40000 ALTER TABLE `Articulos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Articulos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Fabricantes`
+--
+
+DROP TABLE IF EXISTS `Fabricantes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Fabricantes` (
+  `Codigo` int unsigned NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Fabricantes`
+--
+
+LOCK TABLES `Fabricantes` WRITE;
+/*!40000 ALTER TABLE `Fabricantes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Fabricantes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'Tienda_de_Informatica'
+--
+
+--
+-- Dumping routines for database 'Tienda_de_Informatica'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-28 12:34:31
